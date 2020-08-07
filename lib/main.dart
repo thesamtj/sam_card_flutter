322d56606e47815d9cd645de95bbe19a4a52ffc4
Samuel Tijani <omalitijani@gmail.com>
2020-08-07 18:32:50 +0100

    Use different fonts

--- lib/main.dart
+++ lib/main.dart
@@ -21,8 +21,18 @@ class MyApp extends StatelessWidget {
               Text(
                 'Samuel Tijani',
                 style: TextStyle(
+                    fontFamily: 'Pacifico',
                     fontSize: 40.0,
                     color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    letterSpacing: 2.5,
+                    color: Colors.teal.shade100,
                     fontWeight: FontWeight.bold),
               ),
             ],
